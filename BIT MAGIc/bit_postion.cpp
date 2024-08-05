b681e182df14c5a89a3676561e65a3f0c6bb8c1d
SuvitKumar003 <sksuvit2003@gmail.com>
2024-08-05 09:23:44 +0530

    commit

--- BIT MAGIc/bit_postion.cpp
+++ BIT MAGIc/bit_postion.cpp
@@ -1,1 +1,18 @@
-#include<
+#include<iostream>
+using namespace std;
+void check(int n)
+{
+  if(n&1!=0)
+  {
+    cout<<" the first bit is set"<<endl;
+  }
+  else
+  cout<<" the first bit is set"<<endl;
+}
+int main()
+{
+  int n;
+  cout<<" enter the number to be checked "<<endl;
+  cin>>n;
+  check(n);
+}
