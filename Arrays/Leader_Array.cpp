8d955c8ec420e22ad1a102716a0d2756b5646862
SuvitKumar003 <sksuvit2003@gmail.com>
2024-08-16 05:22:39 +0530

    commit

--- Arrays/Leader_Array.cpp
+++ Arrays/Leader_Array.cpp
@@ -2,14 +2,35 @@
 using  namespace std;
 void leader(int arr[],int n)
 {
+  
   for(int i=0;i<n;i++)
   {
+    int count=0;
     for(int j=i;j<n;j++)
     {
-      if(arr[i]>arr[j])
+      
+      if(arr[i]<arr[j])
       {
-        break
+        count++;
       }
     }
+    if(count==0)
+    {
+      cout<<arr[i]<<" ";
+    }
   }
+}
+int main()
+{
+  cout<<"Enter the size of the array: ";
+  int n;
+  cin>>n;
+  cout<<"Enter the elements of the array: ";
+  int arr[n];
+  for(int i=0;i<n;i++)
+  {
+    cin>>arr[i];
+  }
+  leader(arr,n);
+  return 0;
 }
