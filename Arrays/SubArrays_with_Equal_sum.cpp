7570cadd74924b0e51ee4eda6c9826ab01b45a2a
SuvitKumar003 <sksuvit2003@gmail.com>
2024-08-20 19:27:57 +0530

    commit

--- Arrays/SubArrays_with_Equal_sum.cpp
+++ Arrays/SubArrays_with_Equal_sum.cpp
@@ -1,55 +1,67 @@
 #include<iostream>
 using namespace std;
-void index2(int arr[],int n)
+
+void index2(int arr[], int n)
 {
-  int sum=0;
-  for(int i=0;i<n;i++)
-  {
-sum=sum+arr[i];
-  }
-  if(sum%3!=0)
-  {
-return ;
-  }
-  int sum1=0;
-  int sum2=0;
-  sum1=sum/3;
-  int ind1=-1;
-  int ind2=-1;
-  sum2=2*sum1;
-  int x;
-  for(int i=0;i<n;i++)
-  {
-    x+=arr[i];
-    if(sum1==x&& ind1==-1)
+    int sum = 0;
+    
+    // Calculate the total sum of the array
+    for (int i = 0; i < n; i++)
     {
-       ind1=i;
+        sum += arr[i];
     }
-    else if(sum2==x && ind2==-1)
+    
+    // If the sum is not divisible by 3, there can't be two subarrays with equal sums
+    if (sum % 3 != 0)
     {
-      ind2=i;
+        cout << "No such subarray exists." << endl;
+        return;
     }
-  }
-  if(ind1!=-1 && ind2!=-1)
-  {
-    cout<<ind1<<" "<<ind2<<endl;
-  }
-  else
-  {
-    cout<<"No such subarray exists."<<endl;
-  }
+    
+    int sum1 = sum / 3;
+    int sum2 = 2 * sum1;
+    int x = 0;
+    int ind1 = -1;
+    int ind2 = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        x += arr[i];
+        
+        // Check for the first occurrence where cumulative sum equals sum1
+        if (x == sum1 && ind1 == -1)
+        {
+            ind1 = i;
+        }
+        // Check for the first occurrence where cumulative sum equals sum2
+        else if (x == sum2 && ind2 == -1)
+        {
+            ind2 = i;
+        }
+    }
+
+    // If both indices are found, print them
+    if (ind1 != -1 && ind2 != -1)
+    {
+        cout << ind1 << " " << ind2 << endl;
+    }
+    else
+    {
+        cout << "No such subarray exists." << endl;
+    }
 }
+
 int main()
 {
-  int n;
-  cout<<"Enter the size of the array: ";
-  cin>>n;
-  int arr[n];
-  cout<<"Enter the elements of the array: "<<endl;
-  for(int i=0;i<n;i++)
-  {
-    cin>>arr[i];
-  }
-  index2(arr,n);
-  return 0;
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    index2(arr, n);
+    return 0;
 }
