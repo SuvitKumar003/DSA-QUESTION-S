fe495db231549b5bc92bcf7fac59977b1448079f
SuvitKumar003 <sksuvit2003@gmail.com>
2024-10-09 05:37:52 +0530

    commit changes

--- Searching/last_occurance.cpp
+++ Searching/last_occurance.cpp
@@ -1,65 +1,22 @@
 #include<iostream>
 using namespace std;
-
-int findelement(int arr[], int len, int ele)
+int lastoccurance(int arr[],int low,int high,int ele)
 {
-    int l = 0;
-    int h = len - 1;
-    int mid;
-    int result = -1;
-
-    while (l <= h)
+    int mid=(low+high)/2;
+    if(ele<arr[mid])
     {
-        mid = (l + h) / 2;
-
-        if (arr[mid] < ele)
-        {
-            l = mid + 1;
-        }
-        else if (arr[mid] > ele)
-        {
-            h = mid - 1;
-        }
-        else
-        {
-            if (mid == 0 || arr[mid - 1] != arr[mid])
-            {
-                return mid;  // Return the index of the element
-            }
-            else
-            {
-                l=mid+1;  // Continue searching in the left half
-            }
-        }
+        return lastoccurance(arr,low,mid-1,ele);
     }
-    return result;  // Return -1 if the element is not found
-}
-
-int main()
-{
-    int len, element;
-    cout << "Enter the length of the array: " << endl;
-    cin >> len;
-
-    int arr[len];
-    cout << "Enter the elements of the array in sorted order: " << endl;
-    for (int i = 0; i < len; i++)
+    else if(ele>arr[mid])
     {
-        cin >> arr[i];
+        return lastoccurance(arr,mid+1,high,ele);
     }
-
-    cout << "Enter the element to be found: " << endl;
-    cin >> element;
-
-    int result = findelement(arr, len, element);
-    if (result != -1)
+    else if (mid==high || arr[mid]!=arr[mid+1])
     {
-        cout << "Element found at index " << result << endl;  // Output index directly
+        return mid;
     }
     else
     {
-        cout << "Element not found" << endl;
+        return lastoccurance(arr,mid+1,high,ele);
     }
-
-    return 0;
 }
