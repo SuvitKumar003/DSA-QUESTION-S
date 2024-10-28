34134d3e7aeaa887eb2f584d002dbd56c526508f
SuvitKumar003 <sksuvit2003@gmail.com>
2024-10-28 13:13:13 +0530

    commit

---
+++ Sorting/number_of_trainagles.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int number_of_triangles(int arr[],int n)
+{
+  int count=0;
+  for(int i=0;i<n-2;i++)
+  {
+for(int j=i+1;j<n-1;j++)
+{
+  f
+}
+  }
+}
+int main()
+{
+  int arr[]={26,9,27,22,16,27};
+  int s1=sizeof(arr)/sizeof(arr[0]);
+  cout<<number_of_triangles(arr,s1);
+  return 0;
+}
