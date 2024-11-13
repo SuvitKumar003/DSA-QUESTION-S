cafddfc848b96dd6dddd25a2b3d98e2449754813
SuvitKumar003 <sksuvit2003@gmail.com>
2024-11-13 15:30:51 +0530

    commit

--- Hashing/subarray_with_given_sum.cpp
+++ Hashing/subarray_with_given_sum.cpp
@@ -2,68 +2,37 @@
 #include<vector>
 #include<unordered_set>
 using namespace std;
-/*bool subarray_with_given_sum(vector<int> arr,int k)
+bool subarray_with_given_sum(vector<int> arr,int sum)
 {
-  vector<int> prefix_sum;
-  for(int i=0;i<arr.size();i++)
+ unordered_set<int> s;
+ int curr_sum=0;
+ for(int i=0;i<arr.size();i++)
+ {
+  curr_sum+=arr[i];
+  if(curr_sum==sum)
   {
-    if(i==0)
-    {
-      prefix_sum.push_back(arr[i]);
-    }
-    else
-    {
-      prefix_sum.push_back(prefix_sum[i-1]+arr[i]);
-    }
+    return true;
   }
-  for(int i=0;i<arr.size();i++)
+  if(s.find(curr_sum-sum)!=s.end())
   {
-    if(prefix_sum[i]==k)
-    {
-      return true;
-    }
-    else
-    {
-      for(int j=i;j<arr.size();j++)
-      {
-        if(prefix_sum[j]-prefix_sum[i-1]==k)
-        {
-          return true;
-        }
-      }
-    }
-  }
-  return false;
-}*/
-bool issum(vector<int> arr ,int k)
-{
-  int pre_sum=0;
-  unordered_set<int> s;
-  for(int i=0;i<arr.size();i++)
-  {
-    pre_sum+=arr[i];
-    if(pre_sum==k)
-    {
-      return true;
-    }
-    if(s.find(pre_sum-k)!=s.end())
-    {
-      return true;
-    }
-    s.insert(pre_sum);
-  }
-  return false;
-}
-int main()
-{
-  vector<int> arr={1,4,20,3,10,5};
-  int k=33;
-  if(issum(arr,k))
-  {
-    cout<<" Found "<<endl;
+    return true;
   }
   else
   {
-    cout<<" not found "<<endl;
+    s.insert(curr_sum);
+  }
+ }
+ return false;
+}
+int main()
+{
+  vector<int> arr={1,2,3,4,5,6,7,8,9,10};
+  if(subarray_with_given_sum(arr,11))
+  {
+    cout<<"Found the sub array "<<endl;
+  }
+  else
+  {
+    cout<<"Noy found "<<endl;
   }
 }
